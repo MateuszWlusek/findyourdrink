e64ecf58d4bc687bc2a8e14cc11ec5dace739982
Mateusz Włusek <mateusz.wlusek@gmail.com>
2020-07-31 13:23:31 +0200

    Added first look of DrinkRoulette
    Added buttons and test img on screen. Tried with swipe function.

--- src/js/DrinkRoullette.js
+++ src/js/DrinkRoullette.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { useSwipeable } from "react-swipeable";
 const DrinkRoulette = () => {
+  const API = "http://localhost:3005/drinks/";
+
+  const [base, setBase] = useState([]);
+  const [ingr, setIngr] = useState([]);
+  const [drinks, setDrinks] = useState([]);
+  useEffect(() => {
+    fetch(`${API}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setBase(data.bases);
+        setIngr(data.allingr);
+        setDrinks(data.drink);
+      });
+  }, []);
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => console.log("SwipedLeft"),
+    onSwipedRight: () => console.log("SwipedRight"),
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true,
+  });
   return (
     <>
       <nav>
@@ -11,6 +33,11 @@ const DrinkRoulette = () => {
         </ul>
       </nav>
       <h1>Wylosuj drinka</h1>
+      <img src="../img/cocktail-2634115_640.jpg" />
+      <div {...handlers}>
+        <button>Napiję się</button>
+        <button>Nie chcę. Losuj dalej</button>
+      </div>
     </>
   );
 };
