ebefb29a9fd4183117d53d3947878097af24ef34
Mateusz Włusek <mateusz.wlusek@gmail.com>
2020-07-31 12:51:17 +0200

    Add form to SearchDrink module
    Added primal form to section used for searching drinks in base

--- src/js/SearchDrink.js
+++ src/js/SearchDrink.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+
+const API = "http://localhost:3005/drinks/";
+
 const SearchDrink = () => {
+  const [base, setBase] = useState([]);
+  const [ingr, setIngr] = useState([]);
+  const [drinks, setDrinks] = useState([]);
+  useEffect(() => {
+    fetch(`${API}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setBase(data.bases);
+        setIngr(data.allingr);
+        setDrinks(data.drink);
+      });
+  }, []);
+  console.log(drinks[1]);
   return (
     <>
       <nav>
@@ -11,6 +27,28 @@ const SearchDrink = () => {
         </ul>
       </nav>
       <h1>Wyszukaj drinka</h1>
+      <form>
+        <select>
+          {base.map((el) => (
+            <option key={el} value={el}>
+              {el}
+            </option>
+          ))}
+        </select>
+        <select>
+          {ingr.map((el) => (
+            <option key={el} value={el}>
+              {el}
+            </option>
+          ))}
+        </select>
+        <ul>
+          Lista drinków
+          {drinks?.map((e) => (
+            <li key={e.id}>{e.name}</li>
+          ))}
+        </ul>
+      </form>
     </>
   );
 };
