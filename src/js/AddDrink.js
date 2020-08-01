4aecfa09e7794ec3c2fc514ff60de86323a25676
Mateusz Włusek <mateusz.wlusek@gmail.com>
2020-08-01 11:13:26 +0200

    Added form and graphic do adddrink section
    Added few cosmetic options in add drink form. Created graphic design

--- src/js/AddDrink.js
+++ src/js/AddDrink.js
@@ -50,53 +50,72 @@ const AddDrink = () => {
           </li>
         </ul>
       </nav>
-      <h1>Dodaj drinka</h1>
-      <form>
-        <input type="text" />
-        <select>
-          {base.map((el) => (
-            <option key={el} value={el}>
-              {el}
-            </option>
-          ))}
-        </select>
-        <select onChange={addToList}>
-          {ingr.map((el) => (
-            <option key={el} value={el}>
-              {el}
-            </option>
-          ))}
-        </select>
-        <ul>
-          Dodaj składnik do listy
-          {ingrList.map((el) => (
-            <li>
-              <input type="number" name="quan" />
-              <span>ml - </span>
-              <span>{el}</span>
-              <button onClick={addIngredient}>Dodaj składnik</button>
-            </li>
-          ))}
-        </ul>
-        <ul>
-          Lista składników
-          {drinkIngr.map((el) => {
-            return <li>ml - {el}</li>;
-          })}
-        </ul>
-        <textarea
-          type="text"
-          placeholder="Krok po roku dodaj instrukcję"
-          onChange={stepList}
-        />
-        <button onClick={addStep}>Dodaj krok</button>
-        <ul>
-          {instrList.map((e) => {
-            return <li>{e}</li>;
-          })}
-        </ul>
-        <button type="submit">Dodaj drinka!</button>
-      </form>
+      <div className="add-content">
+        <h2>Dodaj drinka</h2>
+        <form>
+          <p>Podaj nazwę drinka</p>
+          <input type="text" />
+          <p>Wybierz alkohol bazowy</p>
+          <select>
+            {base.map((el) => (
+              <option key={el} value={el}>
+                {el}
+              </option>
+            ))}
+          </select>
+          <p>Wybierz dodatkowe składniki</p>
+          <select onChange={addToList}>
+            {ingr.map((el) => (
+              <option key={el} value={el}>
+                {el}
+              </option>
+            ))}
+          </select>
+          <p>Dodaj swój składnik, jeśli nie ma go na liście</p>
+          <input type="text" />
+          <button className="btn-addbase">Dorzuć swój składnik do bazy</button>
+          <p>Dodaj składnik do listy</p>
+          <ul>
+            {ingrList.map((el) => (
+              <li>
+                <input type="number" name="quan" />
+                <span>ml - </span>
+                <span>{el}</span>
+                <button className="btn-addingr" onClick={addIngredient}>
+                  Dodaj składnik
+                </button>
+              </li>
+            ))}
+          </ul>
+          <p>Lista składników</p>
+          <ul>
+            {drinkIngr.map((el) => {
+              return <li>ml - {el}</li>;
+            })}
+          </ul>
+          <div className="text-form">
+            <textarea
+              type="text"
+              placeholder="Krok po roku dodaj instrukcję"
+              cols="40"
+              rows="2"
+              onChange={stepList}
+            />
+            <button className="btn-addstep" onClick={addStep}>
+              Dodaj krok
+            </button>
+          </div>
+
+          <ul>
+            {instrList.map((e) => {
+              return <li>{e}</li>;
+            })}
+          </ul>
+          <button className="btn-adddrink" type="submit">
+            Dodaj drinka!
+          </button>
+        </form>
+      </div>
     </>
   );
 };
